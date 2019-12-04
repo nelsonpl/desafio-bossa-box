e99ecee41662046b8ecd5f39c82cb9969f941d22
Nelson Peixoto Lopes <nelson.npl@gmail.com>
2019-12-04 18:07:44 +0000

    Testando integração com HEROKU.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Desafio BossaBox - Dev. Back-End';
+    return 'Desafio BossaBox - Dev. Back-End - 1.0.0';
   }
 }
