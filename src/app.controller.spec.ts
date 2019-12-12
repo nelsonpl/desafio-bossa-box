bf0ff2becd8ecabe04c1ec000ca5b52673008cc3
Nelson Lopes <nelson.npl@gmail.com>
2019-12-12 11:46:07 -0400

    commit

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,14 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { api } from './api';
-import { AuthModule } from './auth/auth.module';
-import { getModelToken, MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './user/user.model';
-import { AuthService } from './auth/auth.service';
 import { UserService } from './user/user.service';
-import { JwtService } from '@nestjs/jwt';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
-    const userService = new UserService(null);
     const appService = new AppService();
-    const authService = new AuthService(userService, null);
-    appController = new AppController(appService, authService);
+    appController = new AppController(appService);
 
   });
 
